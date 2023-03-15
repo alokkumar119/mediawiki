fe4527d230f9c96ec0e61c2c6119f5786a9e5bb1
alokkumar119 <96578348+alokkumar119@users.noreply.github.com>
2023-03-15 09:41:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -97,3 +97,10 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   depends_on = [azurerm_resource_group.mediawiki]
 }
+
+resource "azurerm_role_assignment" "role_assignemt" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
